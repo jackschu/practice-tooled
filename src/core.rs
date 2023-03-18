fdc899c4fc0774c5d8c6753cbbee78f1d0cd0e6d
jackschu <js9pa@virginia.edu>
2023-03-18 16:09:42 -0400

    give resist_damage it's own test module

--- src/core.rs
+++ src/core.rs
@@ -10,26 +10,28 @@ pub fn resist_damage(raw_damage: f64, resist_amount: f64) -> f64 {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
+	mod resist_damage {
+		use super::*;
+		#[test]
+		fn zero_resist_is_true() {
+			let damage = 1532f64;
+			let resist = 0f64;
+			assert_eq!(damage, resist_damage(damage, resist))
+		}
 
-    #[test]
-    fn zero_resist_is_true() {
-        let damage = 1532f64;
-        let resist = 0f64;
-        assert_eq!(damage, resist_damage(damage, resist))
-    }
+		#[test]
+		fn positive_resist() {
+			let damage = 1000f64;
+			let resist = 25f64;
+			assert_eq!(800f64, resist_damage(damage, resist))
+		}
 
-    #[test]
-    fn positive_resist() {
-        let damage = 1000f64;
-        let resist = 25f64;
-        assert_eq!(800f64, resist_damage(damage, resist))
-    }
-
-    #[test]
-    fn negative_resist() {
-        let damage = 1000f64;
-        let resist = -100f64;
-        assert_eq!(1500f64, resist_damage(damage, resist))
-    }
+		#[test]
+		fn negative_resist() {
+			let damage = 1000f64;
+			let resist = -100f64;
+			assert_eq!(1500f64, resist_damage(damage, resist))
+		}
+	}
 }
