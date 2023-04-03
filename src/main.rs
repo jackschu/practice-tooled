2c284d424e3310ea3462e7a10995cb3a56781273
jackschu <js9pa@virginia.edu>
2023-04-02 20:07:19 -0400

    more refactor

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ mod core;
 mod load_champion;
 
 fn main() {
-    let x = core::resist_damage(400f64, 30f64);
-    println!("Hello, world");
-    println!("{}", x);
-    let sivir = load_champion::load_champion_stats("Sivir");
+    let champion = load_champion::load_champion_stats("Sivir");
 
     let target = attack::Target::new(attack::TargetData {
         base_armor: 30f64,
@@ -15,8 +12,8 @@ fn main() {
 
     let mut dps_vec = vec![];
     for level in 1..19 {
-        let sivir_attack = sivir.as_basic_attack(level);
-        let dps = attack::get_dps(&sivir.as_attack_speed(level), &sivir_attack, &target);
+        let basic_attack = champion.as_basic_attack(level);
+        let dps = attack::get_dps(&champion.as_attack_speed(level), &basic_attack, &target);
         dps_vec.push(dps);
     }
 
