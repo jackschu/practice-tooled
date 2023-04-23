61af543b5e95824fa2395121096cc0ed25f1a27f
jackschu <js9pa@virginia.edu>
2023-04-23 18:13:43 -0400

    add a stats only leblanc impl

---
+++ src/champions/leblanc.rs
@@ -0,0 +1,51 @@
+use crate::{
+    core::stat_at_level,
+    load_champion::{load_champion_stats, ChampionStats},
+};
+
+use super::champion::Champion;
+
+impl Champion for Leblanc {
+    fn get_stats_mut(&mut self) -> &mut ChampionStats {
+        let out = &mut self.stats;
+        return out;
+    }
+    fn get_level(&self) -> u8 {
+        self.level
+    }
+    fn get_stats(&self) -> &ChampionStats {
+        &self.stats
+    }
+    fn get_current_health(&self) -> f64 {
+        self.current_health
+    }
+    fn get_current_health_mut(&mut self) -> &mut f64 {
+        return &mut self.current_health;
+    }
+    fn get_initial_armor(&self) -> f64 {
+        self.initial_armor
+    }
+}
+
+pub struct Leblanc {
+    pub level: u8,
+    pub stats: ChampionStats,
+    initial_armor: f64, // base armor before level ups
+    current_health: f64,
+}
+
+impl Leblanc {
+    const NAME: &str = "Leblanc";
+
+    pub fn new(level: u8) -> Leblanc {
+        let stats = load_champion_stats(Leblanc::NAME);
+        let health = stat_at_level(stats.health, stats.health_per_level, level);
+        let initial_armor = stats.armor;
+        return Leblanc {
+            stats,
+            initial_armor,
+            current_health: health,
+            level,
+        };
+    }
+}
